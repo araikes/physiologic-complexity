7ef25f5c09e70e8ec9c663e86751bfe77b813770
araikes <adam.raikes@gmail.com>
2016-09-19 12:59:59 -0600

    v 0.1
    Add spectral analysis component to work flow

--- spectral.py
+++ spectral.py
@@ -2,22 +2,27 @@ from __future__ import unicode_literals
 import itertools
 import numpy as np
 
-def power_spectrum(ts, Fs):
+def power_spectrum(ts, Fs, norm):
+    if norm:
+        ts = (ts - np.mean(ts))/np.std(ts, ddof=1)
     N = len(ts)
     fftx = np.fft.fft(ts)
 
     N2 = int(N/2)-1
+    result = np.zeros((2, N2))
 
     amp = np.abs(fftx)[1:int(N/2)]
     freq = [i/N2 for i in range(N2)]
     freq = [i*(Fs/2) for i in freq]
 
     power = amp**2
+    result = np.zeros((2, len(freq)))
+    result[0] = freq
+    result[1] = power
 
-    return power
+    return result
 
 def average_power(ts, max_freq, bin_size, norm=False):
-    if norm:
-        ts = (ts - np.mean(ts))/np.std(ts, ddof=1)
+
 
 
