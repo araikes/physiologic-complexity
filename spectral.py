1688f4e803ce9756546346c963f4d44ae2c6f814
araikes <adam.raikes@gmail.com>
2016-11-21 12:31:13 -0700

    Spectral analysis v 0.3

--- spectral.py
+++ spectral.py
@@ -1,31 +1,40 @@
 from __future__ import unicode_literals
-import itertools
 import numpy as np
 
 def power_spectrum(ts, Fs, norm):
+    # Center and standardize time series
     if norm:
         ts = (ts - np.mean(ts))/np.std(ts)
 
     N = len(ts)
     n = np.int(N/2)
 
+    # Use FFT to compute magnitude
     ts_fft = np.abs(np.fft.fft(ts))[1:N]
 
+    # Compute power as the square of the magnitude and retrieve frequencies
+    # Retain only the frequencies from 0 to Nyquist frequency (right-side of frequency spectrum
     ts_pow = (ts_fft**2)[1:n+1]
     ts_freq = (np.arange(0, N-1, 1)/(N*1/Fs))[1:n+1]
 
     return ts_freq, ts_pow
 
-def average_power(ts, Fs, max_freq, bin_size, norm):
-    N = len(ts)
+def average_power(ts, Fs, bin_ends, norm):
+    # Get power spectrum
+    frequencies, power = power_spectrum(ts, Fs, norm)
 
-    frequencies, power = power_spectrum(ts, Fs, norm, one_sided)
+    # Compute indices of frequency bin endpoints
+    bin_indices = sum([[i for i, x in enumerate(frequencies) if x == k] for k in bin_ends], [])
 
-    bins = np.arange(0,max_freq + 1,bin_size)
-    avg_power = np.zeros((1,len(bins)-1))
+    # Normalize the power to max power in the considered range
+    power = power/max(power[0:bin_indices[-1]])
 
+    # Prepare bin_indices for use in computing average power.
+    # To do this, we add 1 to the each value and prepend a 0 to the front of the list.
+    # This way, we compute the sum power from bin_index[i] to (bin_index[i+1] - 1)
+    bin_indices = [x + 1 for x in bin_indices]
+    bin_indices.insert(0,0)
 
-    for i in range(len(bins)-1):
-        avg_power[0,i] = sum(power[np.int(np.floor(bins[i]*N/Fs)):np.int(np.floor(bins[i+1]*N/Fs))])
+    avg_power = [sum(power[bin_indices[i]:bin_indices[i+1]]) for i, x in enumerate(bin_ends)]
 
     return avg_power
